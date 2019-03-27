e50660e7c960225ee3c08816a7561289254d0abf
Ivy Yu <ivys.yu@gmail.com>
2019-03-27 19:28:52 +0800

    added edit and delete options

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -13,6 +13,30 @@ class FlatsController < ApplicationController
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path
+  end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flats_path
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
